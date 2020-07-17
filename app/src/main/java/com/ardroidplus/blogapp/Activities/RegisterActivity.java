825c03847d0f2105ccfdaa0de29e93b2deee8287
Shihab <tomdshihab@gmail.com>
2020-07-17 09:42:57 +0600

    Completed design of register page

--- app/src/main/java/com/ardroidplus/blogapp/Activities/RegisterActivity.java
+++ app/src/main/java/com/ardroidplus/blogapp/Activities/RegisterActivity.java
@@ -1,14 +1,16 @@
-package com.ardroidplus.blogapp;
+package com.ardroidplus.blogapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ardroidplus.blogapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
